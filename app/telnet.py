4bbfaf2f0011587a663c9ba812233db9e3cc448e
henrikcozza <henrique@conza.com.br>
2019-03-05 19:15:21 -0300

    Initial commit

---
+++ app/telnet.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import telnetlib
+
+
+class IOS:
+    def __init__(self, fileHosts=None, host=None):
+        """Incializa construtor armazenando hosts que seram
+        configurados"""
+        if not fileHosts:
+            assert host, "É necessário informar o parametro host ou um " + \
+                    "arquivo\ncontendo todos os hosts utilizados na " + \
+                    "configuração."
+            self.hosts = [host]
+
+        else:
+            with open(fileHosts) as file:
+                self.hosts = file.read().splitlines()
+
+        self.__config = [b'enable\n', b'conf t\n']
+
+    def set_user(self, user, password):
+        """Define variaveis de usuario"""
+        self.__user = {
+            'name': user,
+            'password': password
+        }
+
+    def get_user(self):
+        return self.__user
+
+    def login(self, telnet_obj):
+        user = self.get_user()
+        if user:
+            try:
+                telnet_obj.read_until(b"Username: ")
+                telnet_obj.write(user['name'].encode('ascii') + b"\n")
+                if user['password']:
+                    telnet_obj.read_until(b"Password: ")
+                    telnet_obj.write(user['password'].encode('ascii') + b"\n")
+                else:
+                    print('Password não pode ter valor nulo')
+            except Exception as e:
+                print('Falha ao tentar logar com usuario %s'
+                      '\n Erro %s' % (user['name'], e))
+
+    def addConfig(self, configLine):
+        """Incrementa configurações da instancia para serem
+         executadas nos hosts"""
+        if not isinstance(configLine, bytes):
+            self.__config.append(bytes(configLine))
+        else:
+            self.__config.append(configLine)
+
+    def getConfig(self):
+        """Retorna lista de configureções"""
+        return self.__config
+
+    def configure(self):
+        """Executa comandos que existem no contexto"""
+        config = self.getConfig()
+        for host in self.hosts:
+            host = host.split()
+            if host:
+                try:
+                    tn = telnetlib.Telnet(host)
+
+                    for conf in config:
+                        tn.write(conf)
+                        # finaliza configuração
+                        tn.write(b'end\n')
+                        # desconecta telnet deste host
+                        tn.write(b'exit\n')
+
+                        assert tn.read_all(), "Algo deu errado."
+
+                except Exception as e:
+                    print ('Erro ao tentar conectar'
+                           ' no Host %s\n %s' % (host[0], e))
+
+                return 0
---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="Automato",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Henrique A Conzatti",
+    author_email="henrique@conza.com.br",
+
+    # Packages
+    packages=["app"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    # url="",
+
+    #
+    # license="LICENSE.txt",
+    description="Modulo facilita configurações de devices na rede.",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "telnetlib",
+    ],
+)
